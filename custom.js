62e2a41baca5f61be4191a41eb1c0e308934fdec
pc035860 <pc035860@gmail.com>
2019-12-20 02:23:52 +0800

    Add custom build

---
+++ custom.js
@@ -0,0 +1,21 @@
+export { version } from "./dist/package.js";
+
+export { select, selectAll } from "d3-selection";
+
+export * from "d3-selection-multi";
+export { transition } from "d3-transition";
+
+export {
+  easeElasticOut,
+  easeSinOut,
+  easeBackOut,
+  easeLinear,
+  easeCubicOut,
+  easeExpOut
+} from "d3-ease";
+
+export { arc, pie } from "d3-shape";
+
+export { entries } from "d3-collection";
+
+export { interpolate, interpolateNumber } from "d3-interpolate";
---
+++ rollup-custom.config.js
@@ -0,0 +1,52 @@
+import ascii from "rollup-plugin-ascii";
+import node from "rollup-plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
+import * as meta from "./package.json";
+
+const copyright = `// ${meta.homepage} v${
+  meta.version
+} Copyright ${new Date().getFullYear()} ${meta.author.name}`;
+
+function onwarn(message, warn) {
+  if (message.code === "CIRCULAR_DEPENDENCY") return;
+  warn(message);
+}
+
+export default [
+  {
+    input: "index.js",
+    external: Object.keys(meta.dependencies || {}).filter(key =>
+      /^d3-/.test(key)
+    ),
+    output: {
+      file: "dist/d3-custom.node.js",
+      format: "cjs"
+    },
+    onwarn
+  },
+  {
+    input: "custom.js",
+    plugins: [node(), ascii()],
+    output: {
+      extend: true,
+      banner: copyright,
+      file: "dist/d3-custom.js",
+      format: "umd",
+      indent: false,
+      name: "d3"
+    },
+    onwarn
+  },
+  {
+    input: "custom.js",
+    plugins: [node(), ascii(), terser({ output: { preamble: copyright } })],
+    output: {
+      extend: true,
+      file: "dist/d3-custom.min.js",
+      format: "umd",
+      indent: false,
+      name: "d3"
+    },
+    onwarn
+  }
+];
